56a126944de8f26c5a8a02f186a2483166e8d180
Lijie Huang <huanglijie@outlook.com>
2020-01-11 15:11:05 +0800

    ad face landmarks

--- faceq/demo.py
+++ faceq/demo.py
@@ -6,18 +6,18 @@ import time
 
 import numpy as np
 import cv2
-from PIL import Image
+from PIL import Image, ImageDraw
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
 
-from faceinsight.detection import MTCNNDetector, show_bboxes, show_grids
+from faceinsight.detection import MTCNNDetector
 
 from utils import Config
 from model import Model
 from ui.ui import Ui_Form
 from ui.mouse_event import GraphicsScene
 
-#-- face crop apis
+# face crop apis
 def get_square_crop_box(crop_box, box_scalar=1.0):
     """Get square crop box based on bounding box and the expanding scalar.
     Return square_crop_box and the square length.
@@ -145,6 +145,107 @@ def crop_face(img, bounding_boxes, scalar):
     new_img = new_img.resize((512, 512), Image.BILINEAR)
     
     return [np.array(new_img)]
+
+# face landmarks apis
+def show_grids(img, bounding_boxes, facial_landmarks=[], step=1):
+    """Draw bounding boxes and facial landmarks.
+    Arguments:
+        img: an instance of PIL.Image.
+        bounding_boxes: a float numpy array of shape [n, 5].
+        facial_landmarks: a  float numpy array of shape [n, 10].
+    Returns:
+        an instance of PIL.Image.
+    """
+    img_copy = img.copy()
+    draw = ImageDraw.Draw(img_copy)
+
+    for b in bounding_boxes:
+        draw.rectangle([(b[0], b[1]), (b[2], b[3])],
+                       outline = 'white')
+
+    inx = 0
+    for pp in facial_landmarks:
+        p = pp.reshape(2,5).T
+        p = p.tolist()
+        mouth_center = [(p[3][0] + p[4][0]) / 2, (p[3][1] + p[4][1]) / 2]
+        eye_center = [(p[0][0] + p[1][0]) / 2, (p[0][1] + p[1][1]) / 2]
+        p6 = [(p[2][0] - mouth_center[0])/4 + mouth_center[0],
+              (p[2][1] - mouth_center[1])/4 + mouth_center[1]]
+        p9 = [p[3][0] - (p[4][0]-p[3][0])/3, p[3][1] - (p[4][1]-p[3][1])/3]
+        p10 = [p[4][0] + (p[4][0]-p[3][0])/3, p[4][1] + (p[4][1]-p[3][1])/3]
+        p11 = [mouth_center[0] - (eye_center[0] - mouth_center[0]) / 2,
+               mouth_center[1] - (eye_center[1] - mouth_center[1]) / 2]
+        p12 = [(eye_center[0] -mouth_center[0])/4 + eye_center[0],
+               (eye_center[1] - mouth_center[1])/4 + eye_center[1]]
+        p13 = [(p[0][0] + p[3][0])/2, (p[0][1] + p[3][1])/2]
+        p14 = [(p[1][0] + p[4][0])/2, (p[1][1] + p[4][1])/2]
+
+
+        p.append(p6)
+        p.append([p[0][0]-3/8*(p[1][0]-p[0][0]), 3/2*p[0][1]-1/2*p[1][1]]) 
+        p.append([p[1][0]+3/8*(p[1][0]-p[0][0]), 3/2*p[1][1]-1/2*p[0][1]])
+        p.append(p9)
+        p.append(p10)
+        p.append(p11)        
+        p.append(p12)
+        p.append(p13)
+        p.append(p14)
+
+
+        #for i in range(12):
+        #    draw.ellipse([
+        #         (p[i][0]-2.0,p[i][1]-2.0),
+        #         (p[i][0]+2.0,p[i][1]+2.0)
+        #     ],outline='white',fill='white')
+
+        #draw.ellipse(
+        #    [(p[1][0]-30.0, p[1][1]-30.0),
+        #     (p[1][0]+30.0, p[1][1]+30.0)],
+        #    outline=(136,232,232),
+        #    width=5
+        #)
+
+        draw.line(
+            ((p[6][0], p[6][1]),
+             (p[0][0], p[0][1]),
+             (p[12][0], p[12][1]),
+             (p[5][0], p[5][1]),
+             (p[13][0],p[13][1]),
+             (p[1][0], p[1][1]),
+             (p[7][0], p[7][1])),
+            fill=(136,232,232),
+            width=1
+        )
+
+        draw.line(
+            ((p[11][0], p[11][1]),
+             (p[7][0], p[7][1]),
+             (p[9][0], p[9][1]),
+             (p[10][0], p[10][1]),
+             (p[8][0], p[8][1]),
+             (p[6][0], p[6][1]),
+             (p[11][0], p[11][1])),
+            fill=(136,232,232),
+            width=1
+        )
+
+        draw.line(
+            ((p[11][0], p[11][1]),
+             (p[1][0], p[1][1]),
+             (p[2][0], p[2][1]),
+             (p[5][0], p[5][1]),
+             (p[4][0], p[4][1]),
+             (p[10][0], p[10][1]),
+             (p[3][0], p[3][1]),
+             (p[5][0], p[5][1]),
+             (p[2][0], p[2][1]),
+             (p[0][0], p[0][1]),
+             (p[11][0], p[11][1])),
+            fill=(136,232,232),
+            width=1
+        )
+
+        return img_copy
 
 
 class RecordVideo(QtCore.QObject):
@@ -305,22 +406,22 @@ class Ex(QtWidgets.QWidget, Ui_Form):
             return
 
         # draw landmarks on display image
-        _face_img = Image.fromarray(faces[0][:, :, ::-1])
-        _, landmarks = self.detector.infer(_face_img, min_face_size=100)
-        #_face_img = show_bboxes(_face_img, [], landmarks)
-        _face_img = show_grids(_face_img, [], landmarks)
-        _face_img = np.array(_face_img)[:, :, ::-1]
+        #_face_img = Image.fromarray(faces[0][:, :, ::-1])
+        #_, landmarks = self.detector.infer(_face_img, min_face_size=100)
+        ##_face_img = show_bboxes(_face_img, [], landmarks)
+        #_face_img = show_grids(_face_img, [], landmarks)
+        #_face_img = np.array(_face_img)[:, :, ::-1]
 
         # convert data frame into QImage
         self._frame_data = faces[0]
         h, w, c = self._frame_data.shape
         bytes_per_line = 3 * w
-        #_frame_image = QtGui.QImage(self._frame_data.data, w, h,
-        #                            bytes_per_line,
-        #                            QtGui.QImage.Format_RGB888)
-        _frame_image = QtGui.QImage(_face_img.copy(), w, h,
+        _frame_image = QtGui.QImage(self._frame_data.data, w, h,
                                     bytes_per_line,
                                     QtGui.QImage.Format_RGB888)
+        #_frame_image = QtGui.QImage(_face_img.copy(), w, h,
+        #                            bytes_per_line,
+        #                            QtGui.QImage.Format_RGB888)
         _frame_image = _frame_image.rgbSwapped()
         image = QtGui.QPixmap.fromImage(_frame_image)
         mat_img = self._frame_data
